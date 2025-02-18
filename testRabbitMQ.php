1593bf2a8948a14230e26d5ba5a2ed334d219de3
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2025-02-17 23:24:48 -0500

    Update testRabbitMQ.php

--- testRabbitMQ.php
+++ testRabbitMQ.php
@@ -4,4 +4,14 @@ require __DIR__ . '/vendor/autoload.php';
 
 use RPurinton\RabbitMQ;
 
+RabbitMQ::publish('rabbitmq-test', 'Hello, world!');
 
+$mq = new RabbitMQ('rabbitmq-test', function (string $message): void {
+    if ($message === 'Hello, world!') {
+        echo "Success!\n";
+    } else {
+        echo "Failure?!\n";
+        echo "Expected 'Hello, world!', got '$message'\n";
+    }
+    die();
+});
