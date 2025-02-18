726807baef5a137c5d69e1b107d4f32f7be831c9
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2025-02-17 23:32:01 -0500

    Testing

--- src/RabbitMQ.php
+++ src/RabbitMQ.php
@@ -44,6 +44,17 @@ class RabbitMQ
     }
 
     /**
+     * RabbitMQ destructor.
+     *
+     * @return void
+     */
+    public function __destruct()
+    {
+        $this->loop->stop();
+    }
+
+
+    /**
      * Obtains a channel from an asynchronous client.
      *
      * @param AsyncClient $client The asynchronous client instance.
--- testRabbitMQ.php
+++ testRabbitMQ.php
@@ -3,18 +3,16 @@
 require __DIR__ . '/vendor/autoload.php';
 
 use RPurinton\RabbitMQ;
-use React\EventLoop\Loop;
 
 RabbitMQ::publish('rabbitmq-test', 'Hello, world!');
 
-$loop = Loop::get();
-
-$mq = new RabbitMQ('rabbitmq-test', function (string $message) use ($loop): void {
+$mq = new RabbitMQ('rabbitmq-test', function (string $message): void {
     if ($message === 'Hello, world!') {
         echo "Success!\n";
+        exit(0);
     } else {
         echo "Failure?!\n";
         echo "Expected 'Hello, world!', got '$message'\n";
+        exit(1);
     }
-    $loop->stop();
-}, $loop);
+});
