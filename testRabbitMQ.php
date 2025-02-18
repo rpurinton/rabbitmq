02493adb1556301857a2aab28b687f1d1c41fc02
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2025-02-17 23:35:05 -0500

    Revert "Testing"
    This reverts commit e80f5cda5faafb7f8b285c1e94a916160f2c0f35.

--- testRabbitMQ.php
+++ testRabbitMQ.php
@@ -3,20 +3,16 @@
 require __DIR__ . '/vendor/autoload.php';
 
 use RPurinton\RabbitMQ;
-use React\EventLoop\Loop;
 
 RabbitMQ::publish('rabbitmq-test', 'Hello, world!');
 
-$loop = Loop::get();
-$mq = new RabbitMQ('rabbitmq-test', function (string $message) use ($loop): void {
+$mq = new RabbitMQ('rabbitmq-test', function (string $message): void {
     if ($message === 'Hello, world!') {
         echo "Success!\n";
-        $loop->stop();
         exit(0);
     } else {
         echo "Failure?!\n";
         echo "Expected 'Hello, world!', got '$message'\n";
-        $loop->stop();
         exit(1);
     }
-}, $loop);
+});
