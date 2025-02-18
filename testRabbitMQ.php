5fd121b9183b4f321394a429fa3b456ec790b891
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2025-02-17 23:36:26 -0500

    Testing

--- testRabbitMQ.php
+++ testRabbitMQ.php
@@ -12,9 +12,12 @@ $loop = Loop::get();
 $mq = new RabbitMQ('rabbitmq-test', function (string $message) use ($loop): void {
     if ($message === 'Hello, world!') {
         echo "Success!\n";
+        $exitCode = 0;
     } else {
         echo "Failure?!\n";
         echo "Expected 'Hello, world!', got '$message'\n";
+        $exitCode = 1;
     }
     $loop->stop();
+    exit($exitCode);
 }, $loop);
