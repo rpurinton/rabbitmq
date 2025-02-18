e80f5cda5faafb7f8b285c1e94a916160f2c0f35
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2025-02-17 23:34:03 -0500

    Testing

--- testRabbitMQ.php
+++ testRabbitMQ.php
@@ -3,16 +3,20 @@
 require __DIR__ . '/vendor/autoload.php';
 
 use RPurinton\RabbitMQ;
+use React\EventLoop\Loop;
 
 RabbitMQ::publish('rabbitmq-test', 'Hello, world!');
 
-$mq = new RabbitMQ('rabbitmq-test', function (string $message): void {
+$loop = Loop::get();
+$mq = new RabbitMQ('rabbitmq-test', function (string $message) use ($loop): void {
     if ($message === 'Hello, world!') {
         echo "Success!\n";
+        $loop->stop();
         exit(0);
     } else {
         echo "Failure?!\n";
         echo "Expected 'Hello, world!', got '$message'\n";
+        $loop->stop();
         exit(1);
     }
-});
+}, $loop);
