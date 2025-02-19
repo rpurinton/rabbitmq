8ee2edcae85d7d46fd55c5f569d1b1babdb67ab5
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2025-02-19 17:19:25 -0500

    Update testRabbitMQ.php

--- testRabbitMQ.php
+++ testRabbitMQ.php
@@ -3,17 +3,19 @@
 require __DIR__ . '/vendor/autoload.php';
 
 use RPurinton\RabbitMQ;
-use React\EventLoop\{Loop, LoopInterface};
+use React\EventLoop\Loop;
 
 RabbitMQ::publish('rabbitmq-test', 'Hello, world!');
 
 $loop = Loop::get();
-RabbitMQ::connect('rabbitmq-test', function (string $message) use ($loop) : bool {
+RabbitMQ::connect('rabbitmq-test', function (string $message) use ($loop): bool {
     if ($message === 'Hello, world!') {
         echo "Success!\n";
     } else {
         echo "Failure?!\nExpected 'Hello, world!', got '$message'\n";
     }
-    echo ("Press CTRL+C to exit.\n");
+    $loop->futureTick(function () use ($loop) {
+        $loop->stop();
+    });
     return true;
-});
+}, $loop);
