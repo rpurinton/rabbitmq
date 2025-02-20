95c5d35adceb3e333fe25495176d54aedfd82645
Russell Purinton <russell.purinton@icloud.com>
2025-02-20 00:30:54 +0000

    Updated 2025-02-20

--- testRabbitMQ.php
+++ testRabbitMQ.php
@@ -5,17 +5,12 @@ require __DIR__ . '/vendor/autoload.php';
 use RPurinton\RabbitMQ;
 use React\EventLoop\Loop;
 
-RabbitMQ::publish('rabbitmq-test', 'Hello, world!', false, false, false, true);
+RabbitMQ::publish('rabbitmq-test', 'Hello, world!');
 
 $loop = Loop::get();
 RabbitMQ::connect('rabbitmq-test', function (string $message) use ($loop): bool {
-    if ($message === 'Hello, world!') {
-        echo "Success!\n";
-    } else {
-        echo "Failure?!\nExpected 'Hello, world!', got '$message'\n";
-    }
-    $loop->futureTick(function () use ($loop) {
-        $loop->stop();
-    });
+    if ($message === 'Hello, world!') echo "Success!\n";
+    else echo "Failure?!\nExpected 'Hello, world!', got '$message'\n";
+    $loop->futureTick(function () use ($loop) { $loop->stop(); });
     return true;
-}, $loop, false, false, false, true);
+}, $loop);
